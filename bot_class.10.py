979387b5b3e840b9dbb86d3a3d260a0c2fe0f052
Іван Лубик <126516121+ivanlubyk@users.noreply.github.com>
2023-04-07 17:37:36 +0300

    Update bot_class.10.py

--- bot_class.10.py
+++ bot_class.10.py
@@ -1,28 +1,23 @@
 from collections import UserDict
 
 
-class Name:
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return self.value
-
-
-class Phone:
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return self.value
-
-
 class Field:
     def __init__(self, value):
         self.value = value
 
     def __str__(self):
         return self.value
+
+
+class Name(Field):
+    pass
+
+
+class Phone:
+    def __init__(self, name, phone=None):
+        self.name = name
+        self.phones = [phone] if phone else []
+
 
 
 class Record:
@@ -58,7 +53,19 @@ class AddressBook(UserDict):
                 result.append(record)
         return result
 
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError as e:
+            print(f"Invalid input: KeyError '{e.args[0]}'")
+        except ValueError as e:
+            print(f"Invalid input: ValueError '{e.args[0]}'")
+        except IndexError as e:
+            print(f"Invalid input: IndexError '{e.args[0]}'")
+    return inner
 
+@input_error
 def add_contact():
     name_value = input("Enter name: ")
     name = Name(name_value)
@@ -69,7 +76,7 @@ def add_contact():
     address_book.add_record(record)
     print(f"Contact {str(name)} with phone {str(phone)} has been added.")
 
-
+@input_error
 def change_contact():
     name_value = input("Enter name: ")
     name = Name(name_value)
@@ -83,6 +90,7 @@ def change_contact():
     print(f"Phone for contact {str(name)} has been changed to {str(phone)}.")
 
 
+@input_error
 def show_phone():
     name_value = input("Enter name: ")
     name = Name(name_value)
@@ -91,33 +99,62 @@ def show_phone():
         print("Contact not found.")
         return
     print(f"{str(name)}: {str(record.phones[0])}")
+    
+    
+@input_error
+def remove_contact():
+    name_value = input("Enter name: ")
+    name = Name(name_value)
+    if str(name) in address_book.data:
+        del address_book.data[str(name)]
+        print(f"Contact {str(name)} has been removed.")
+    else:
+        print("Contact not found.")
 
 
+@input_error
 def give_me_all():
-    query = input("Enter query: ")
+    query = input("Enter remove: ")
     records = address_book.find_record(query)
     if not records:
         print("No matching contacts found.")
     else:
         for record in records:
             print(record)
+            
+            
+            
+def handler(command):
+    if command.startswith("add"):  
+            add_contact()
+    elif command.startswith("change"):
+            change_contact()
+    elif command.startswith("phone"):
+        show_phone()
+    elif command.startswith("show all"):
+        give_me_all()
+    else:
+        print("Input error: Invalid command. Please try again.")
 
 bye = ["good bye", "close", "exit"]
 hel = ["hi","hello"]
 
+
 address_book = AddressBook()
-while True:
-    action = input("Welcome app >>> action: (add, change, phone, find, exit): ").lower()
-    if action in hel:
-        print("How can I help you?")
-    elif action == "add":
-        add_contact()
-    elif action == "change":
-        change_contact()
-    elif action == "phone":
-        show_phone()
-    elif action == "find":
-        give_me_all()
-    elif action in bye:
-        break
+
+def primitive_bot():
+    while True:
+        action = input("Welcome app to start work enter <hello> or <hi>\nTo finish, enter > (good bye, close, exit):" ).lower()
+        if action in hel:
+            print("How can I help you?\nThe following commands are available to you,\n(add, change, phone, remove, find, exit)")    
+        elif action in bye:
+            print("Good bye!")
+            break
+        else:
+            handler(action)
+
+if __name__ == "__main__":
+    primitive_bot()
+    
+       
    
